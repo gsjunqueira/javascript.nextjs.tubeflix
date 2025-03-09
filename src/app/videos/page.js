e23bee63a7c8a478cc84e4e157284fe0b4fdbc81
gsjunqueira <giovanisjunqueira@gmail.com>
2025-03-09 16:46:31 -0300

    HighlightVideo

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -1,9 +1,10 @@
+import HighlightVideo from '@/components/HighlightVideo'
 import styles from './page.module.css'
 
 export default function VideosPage() {
     return (
         <>
-            <h2 className={styles.title}>Vídeo</h2>
+            <HighlightVideo tags='Vídeos' />
         </>
     )
 }
