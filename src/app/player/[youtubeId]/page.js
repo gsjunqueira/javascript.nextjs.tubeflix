de8aa5f4739fa1aeb9418cdb8c2d7d4b31ad24a9
gsjunqueira <giovanisjunqueira@gmail.com>
2025-03-10 09:24:20 -0300

    player parametrizavel

--- src/app/player/[youtubeId]/page.js
+++ src/app/player/[youtubeId]/page.js
@@ -3,12 +3,14 @@
 
 import YouTube from 'react-youtube'
 import style from './page.module.css'
+import { useParams } from 'next/navigation'
 
 export default function PlayerPage() {
+    const { youtubeId } = useParams();
     return (
         <div className={ style.player }>
             <YouTube 
-                videoId='yZLwd_WobSA' 
+                videoId={youtubeId} 
                 opts={{
                     playerVars: {
                         autoplay: 1,
